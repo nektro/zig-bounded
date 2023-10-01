d118fff073e5c6b978c4b976e01c6cb461d4aaa1
Meghan Denny <hello@nektro.net>
2023-10-01 16:07:05 -0700

    test: add comments for what the inner representation is

--- test.zig
+++ test.zig
@@ -21,36 +21,36 @@ fn expect(
 }
 
 test {
-    const x = bounded.constant(10);
+    const x = bounded.constant(10); //0
     const X = @TypeOf(x);
     try std.testing.expect(X.Repr == u0);
     try expect(x, 10, 10, 0, 10);
 }
 
 test "int.add" {
-    const x = bounded.int(1, 100).from(42);
+    const x = bounded.int(1, 100).from(42); //41
     const y = bounded.int(-3, 7).from(2);
     const z = x.add(y);
     try expect(z, -2, 107, 46, 44);
 }
 
 test "int.add minimum" {
-    const x = bounded.int(1, 100).from(1);
-    const y = bounded.int(-3, 7).from(-3);
+    const x = bounded.int(1, 100).from(1); //0
+    const y = bounded.int(-3, 7).from(-3); //0
     const z = x.add(y);
     try expect(z, -2, 107, 0, -2);
 }
 
 test "int.add maximum" {
-    const x = bounded.int(1, 100).from(100);
-    const y = bounded.int(-3, 7).from(7);
+    const x = bounded.int(1, 100).from(100); //99
+    const y = bounded.int(-3, 7).from(7); //10
     const z = x.add(y);
     try expect(z, -2, 107, 109, 107);
 }
 
 test "int.add const" {
-    const x = bounded.constant(10);
-    const y = bounded.constant(20);
+    const x = bounded.constant(10); //0
+    const y = bounded.constant(20); //0
     const z = x.add(y);
     try expect(z, 30, 30, 0, 30);
 }
