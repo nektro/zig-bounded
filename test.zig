5e78439a735deab8a81fbd71ba82325f7e5752cb
Meghan Denny <hello@nektro.net>
2023-10-01 16:06:46 -0700

    test: collapse expect() calls

--- test.zig
+++ test.zig
@@ -24,63 +24,33 @@ test {
     const x = bounded.constant(10);
     const X = @TypeOf(x);
     try std.testing.expect(X.Repr == u0);
-    try expect(
-        x,
-        10,
-        10,
-        0,
-        10,
-    );
+    try expect(x, 10, 10, 0, 10);
 }
 
 test "int.add" {
     const x = bounded.int(1, 100).from(42);
     const y = bounded.int(-3, 7).from(2);
     const z = x.add(y);
-    try expect(
-        z,
-        -2,
-        107,
-        46,
-        44,
-    );
+    try expect(z, -2, 107, 46, 44);
 }
 
 test "int.add minimum" {
     const x = bounded.int(1, 100).from(1);
     const y = bounded.int(-3, 7).from(-3);
     const z = x.add(y);
-    try expect(
-        z,
-        -2,
-        107,
-        0,
-        -2,
-    );
+    try expect(z, -2, 107, 0, -2);
 }
 
 test "int.add maximum" {
     const x = bounded.int(1, 100).from(100);
     const y = bounded.int(-3, 7).from(7);
     const z = x.add(y);
-    try expect(
-        z,
-        -2,
-        107,
-        109,
-        107,
-    );
+    try expect(z, -2, 107, 109, 107);
 }
 
 test "int.add const" {
     const x = bounded.constant(10);
     const y = bounded.constant(20);
     const z = x.add(y);
-    try expect(
-        z,
-        30,
-        30,
-        0,
-        30,
-    );
+    try expect(z, 30, 30, 0, 30);
 }
