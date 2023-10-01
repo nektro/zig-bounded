d58e7857072af43ade688b9d8e9d7a03bb378b12
Meghan Denny <hello@nektro.net>
2023-10-01 14:59:40 -0700

    simplify tests

--- test.zig
+++ test.zig
@@ -1,65 +1,88 @@
 const std = @import("std");
 const bounded = @import("bounded");
 
+fn expect(
+    z: anytype,
+    comptime z_min: comptime_int,
+    comptime z_max: comptime_int,
+    comptime z_repr: comptime_int,
+    comptime z_real: comptime_int,
+    comptime z_fmt: []const u8,
+) !void {
+    const Z = @TypeOf(z);
+    try std.testing.expect(Z.min == z_min);
+    try std.testing.expect(Z.max == z_max);
+    try std.testing.expect(z.repr == z_repr);
+    try std.testing.expect(z.real() == z_real);
+    try std.testing.expectFmt(z_fmt, "{}", .{z});
+}
+
 test {
     const x = bounded.constant(10);
     const X = @TypeOf(x);
-    try std.testing.expect(X.min == 10);
-    try std.testing.expect(X.max == 10);
     try std.testing.expect(X.Repr == u0);
-    try std.testing.expect(x.repr == 0);
-    try std.testing.expect(x.real() == 10);
-    try std.testing.expectFmt("10", "{}", .{x});
+    try expect(
+        x,
+        10,
+        10,
+        0,
+        10,
+        "10",
+    );
 }
 
 test "int.add" {
     const x = bounded.int(1, 100).from(42);
     const y = bounded.int(-3, 7).from(2);
     const z = x.add(y);
-    const Z = @TypeOf(z);
-
-    try std.testing.expect(Z.min == -2);
-    try std.testing.expect(Z.max == 107);
-    try std.testing.expect(z.repr == 46);
-    try std.testing.expect(z.real() == 44);
-    try std.testing.expectFmt("44", "{}", .{z});
+    try expect(
+        z,
+        -2,
+        107,
+        46,
+        44,
+        "44",
+    );
 }
 
 test "int.add minimum" {
     const x = bounded.int(1, 100).from(1);
     const y = bounded.int(-3, 7).from(-3);
     const z = x.add(y);
-    const Z = @TypeOf(z);
-
-    try std.testing.expect(Z.min == -2);
-    try std.testing.expect(Z.max == 107);
-    try std.testing.expect(z.repr == 0);
-    try std.testing.expect(z.real() == -2);
-    try std.testing.expectFmt("-2", "{}", .{z});
+    try expect(
+        z,
+        -2,
+        107,
+        0,
+        -2,
+        "-2",
+    );
 }
 
 test "int.add maximum" {
     const x = bounded.int(1, 100).from(100);
     const y = bounded.int(-3, 7).from(7);
     const z = x.add(y);
-    const Z = @TypeOf(z);
-
-    try std.testing.expect(Z.min == -2);
-    try std.testing.expect(Z.max == 107);
-    try std.testing.expect(z.repr == 109);
-    try std.testing.expect(z.real() == 107);
-    try std.testing.expectFmt("107", "{}", .{z});
+    try expect(
+        z,
+        -2,
+        107,
+        109,
+        107,
+        "107",
+    );
 }
 
 test "int.add const" {
     const x = bounded.constant(10);
     const y = bounded.constant(20);
     const z = x.add(y);
-    const Z = @TypeOf(z);
-
-    try std.testing.expect(Z.min == 30);
-    try std.testing.expect(Z.max == 30);
-    try std.testing.expect(z.repr == 0);
-    try std.testing.expect(z.real() == 30);
-    try std.testing.expectFmt("30", "{}", .{z});
+    try expect(
+        z,
+        30,
+        30,
+        0,
+        30,
+        "30",
+    );
 }
